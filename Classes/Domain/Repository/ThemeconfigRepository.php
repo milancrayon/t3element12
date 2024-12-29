671f2aaa1d25659643c07108f3f3d8e8d7eaf04b
Milan <milan.crayon@gmail.com>
2024-12-29 18:24:18 +0530

    Updates

--- Classes/Domain/Repository/ThemeconfigRepository.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace Crayon\T3element\Domain\Repository;
-class ThemeconfigRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
-{   
-}
--- ext_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-defined('TYPO3') or die();
-use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-  
-ExtensionManagementUtility::allowTableOnStandardPages('tx_t3element_domain_model_elements');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_t3element_domain_model_license');
-ExtensionManagementUtility::allowTableOnStandardPages('tx_t3element_domain_model_themeconfig');
